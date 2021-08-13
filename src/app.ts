4a6130625d6d5755f6c12b0bf37f69fd711b7e57
dhiegodias <ddias003@gmail.com>
2021-08-13 00:31:15 -0300

    Installed Linter, fJest, fixed warnings and created routes index

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
-import "reflect-metadata";
+import 'reflect-metadata';
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { createConnection } from 'typeorm';
+import routes from './routes/index';
 
-console.log('Hello world!')
+createConnection().then(() => {
+  dotenv.config();
+
+  const app = express();
+  const port = process.env.PORT;
+
+  app.use(express.json());
+  app.use(routes);
+
+  app.listen(port);
+});
